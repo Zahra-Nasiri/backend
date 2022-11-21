0714df2e8686cfd82dfb0d707f8a441fbadc352e
Zahra-Nasiri <z.nasir.8181@gmail.com>
2022-11-21 16:12:07 +0330

    customizing admin panel for event and registrants model

--- base/admin.py
+++ base/admin.py
@@ -1,5 +1,20 @@
 from django.contrib import admin
 from .models import Event, Registrant
 # Register your models here.
-admin.site.register(Event)
-admin.site.register(Registrant)
+@admin.register(Event)
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['type','title', 'owner', 'held',
+     'location', 'capacity', 'enrolled', 'date_time']
+    list_filter = ['type', 'owner', 'held']
+    search_fields = ['type', 'title', 'owner__username',
+     'held', 'location', 'capacity']
+    ordering = ['date_time', 'held']
+
+@admin.register(Registrant)
+class RegistrantAdmin(admin.ModelAdmin):
+    list_display = ['first_name', 'last_name', 'event',
+     'student_id', 'university', 'phone_number']
+    list_filter = ['event__type']
+    search_fields = ['first_name', 'last_name', 'event__type',
+     'event__title', 'student_id', 'university']
+    ordering = ['event__date_time']    
